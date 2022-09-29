fdb003f01fca8501a3701b216957cdbd32e8cea6
q-sw <quentin.swiech@gmail.com>
2022-09-29 17:28:25 +0200

    add script to init terraform project

---
+++ create_tf_module.py
@@ -0,0 +1,10 @@
+import os
+
+TERRAFORM_FILE = ["main.tf", "variables.tf", "outputs.tf", "versions.tf","README.md" ]
+
+current_path = os.getcwd()
+for tf_file in TERRAFORM_FILE:
+	file_path = os.path.join(current_path, tf_file)
+	if not os.path.exists(file_path):
+		f = open(file_path, 'x')
+		f.close
